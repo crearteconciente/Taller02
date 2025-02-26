4cc494cdf01ff76c166921d3f779fdb919c48bcb
Andres <crearteconciente@gmail.com>
2025-02-26 11:12:05 -0500

    archivos para verificar pruebas

---
+++ run_test.py
@@ -0,0 +1,9 @@
+from test.tests import test_validar_usuario
+from test.tests import test_calcular_puntuacion
+from test.tests import test_validar_email
+from test.tests import test_validar_telefono
+
+test_validar_usuario()
+test_calcular_puntuacion()
+test_validar_email()
+test_validar_telefono()
